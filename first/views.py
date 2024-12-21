78503c2a3a5eb102d027923fd65167811d9986c1
anna zmievets <annzm1tz@gmail.com>
2024-12-21 16:24:45 +0400

    Django 0.2

--- first/views.py
+++ first/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+
+def index_page(request):
+    context = {
+        'name': 'Курс "Промышленное программирование"',
+        'author': 'Аня Змиевец',
+        'page_count': 3,
+    }
+    return render(request, 'index.html', context)
